521d7a79531a35ffe01b2118051ad976c7b1e2f5
Kazuki H <58931401+Kaz205@users.noreply.github.com>
2022-09-15 22:33:12 +0900

    Silence tar

--- clang.sh
+++ clang.sh
@@ -8,5 +8,5 @@ export CLANG_VERSION=clang-$1
 rm -rf linux-x86
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b master --depth=1 -j$(nproc --all)
 cd linux-x86
-tar -C . -cvf $CLANG_VERSION.tar $CLANG_VERSION
+tar -C . -cf $CLANG_VERSION.tar $CLANG_VERSION
 xz -e -T8 -9 $CLANG_VERSION.tar
